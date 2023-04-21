787569bed537b978c7be88fdabb7eef463a4d618
Matthew Jakeman <mjakeman26@outlook.co.nz>
2023-04-22 02:13:03 +1200

    rename from mtools to tools

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col gap-4 w-full min-h-screen bg-zinc-50 max-w-screen-md p-16 m-auto">
       <header>
-        <p className='text-black/50'><a className="underline" href="https://mattjakeman.com/">mattjakeman.com</a> / mtools</p>
+        <p className='text-black/50'><a className="underline" href="https://mattjakeman.com/">mattjakeman.com</a> / tools</p>
       </header>
       <EnumConverter />
     </div>
